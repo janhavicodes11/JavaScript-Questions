ab9489d041b3146d5cb28ddd1cd6bdb03c180880
Janhavi <janhavi.hariom2026@gmail.com>
2026-06-10 14:27:34 +0530

    Add files via upload

---
+++ Phase-1/console-basics/script.js
@@ -0,0 +1,27 @@
+//Question-1 Print "Hello JavaScript" in the console.
+console.log("Hello JavaScript")
+
+
+
+
+//Question- 2 : Print your name, age, and city using one console.log().
+let name= "Janhavi"
+let age= 19
+let city="Mumbai"
+console.log(name, age, city)
+
+
+
+// Question- 3 : Print a warning message using console.warn().
+console.warn("Open if you are 18+ only")
+
+
+
+// Question- 4 : Print an error message using console.error().
+console.error("This is an error")
+
+
+
+// Question-5 : Use console.table() to display an array of 5 numbers.
+var arr=[10,20,40,50,90]
+console.table(arr)
---
+++ Phase-1/variables-datatypes/script.js
@@ -0,0 +1,50 @@
+// Question 1 : Create a variable called age, store your age in it, and print a sentence like: "My age is 19 years."
+var age=19
+console.log(`My age is ${age}`)
+
+
+// Question-2 : Create variables for your city and country and print them in one sentence.
+var city= "Mumbai"
+var country= "India"
+console.log(`Aamchi ${city} in country ${country}`)
+
+// Question- 4 : Create two variables containing numbers and swap their values without manually changing them.
+var num1= 20
+var num2= 30
+/*console.log(num1=num2)
+console.log(num2=num1)*/
+var temp=num1;
+num1=num2
+num2=temp
+console.log(num1)
+console.log(num2)
+
+
+
+
+// Question-5 : Create a variable containing your favorite movie name and use typeof to display its data type.
+let name="Breasts and eggs"
+console.log(typeof(name))
+
+
+
+
+// Question-6 : Create a variable containing a decimal number and display its data type.
+let num=12.8908
+console.log(typeof(num))
+
+// Question-7 : Create a variable containing a Boolean value and display its data type.
+var name1= true
+console.log(typeof(name1))
+
+// Question-8 : Declare a variable without assigning any value and print its value and type.
+var num3
+console.log(typeof(num3))
+
+
+
+
+// Question-9 : Declare a variable without assigning any value and print its value and type.
+var numm=null
+console.log(numm)
+console.log(typeof(numm))
