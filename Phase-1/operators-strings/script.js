dbcfdf2ee899c6098c3d7d13d4677dd71e8b5b6f
Janhavi <janhavi.hariom2026@gmail.com>
2026-06-10 18:37:53 +0530

    Add files via upload

---
+++ Phase-1/operators-strings/script.js
@@ -0,0 +1,163 @@
+// Question-16 : Create two numbers and print their sum.
+/*var num1 = Number(prompt("Enter num 1"))
+var num2 = Number(prompt("Enter num 2"))
+
+console.log(num1 + num2)*/
+
+
+
+
+// Question-17 : Create two numbers and print their difference.
+/*var num1= Number(prompt("Enter num 1 : "))
+var num2= Number(prompt(" Enter num 2 :"))
+console.log(num1 - num2)*/
+
+
+
+
+// Question-18 : Create two numbers and print their  product.
+/*let num1= Number(prompt("Enter num1"))
+let num2= Number(prompt("Enter num2"))
+console.log(num1 * num2)*/
+
+
+
+
+// Question-19 : Create two numbers and print their quotient.
+/*let num1= Number(prompt("Enter num 1 :"))
+ let num2= Number (prompt("Enter num 2 :"))
+ console.log(num1 / num2)*/
+
+
+ // Question-20 : Find the remainder when 29 is divided by 4.
+ /*let num1= 29
+ let num2=4
+ console.log(num1%num2)*/
+
+
+
+
+ // Question-21 : Find the square of a number using the exponent operator.
+ /*let num=Number(prompt("Enter num 1"))
+ console.log(num**2)*/
+
+
+
+
+ // Question-22 : Use post-increment on a variable and print the result.
+ /*let a=10
+ console.log(a++)
+ console.log(a)*/
+
+
+
+ // Question-23 : Use pre-increment on a variable and print the result.
+ /*let b=20
+ console.log(++b)
+ console.log(b)*/
+
+
+
+
+
+ // Question 24- Use the += operator to increase a variable by 10.
+ /*let a= 10
+ a+=10 
+ console.log(a)*/
+
+
+
+
+ // Question-25 : Use the *= operator to double a variable's value 
+ /*let b=10 
+ b*=10
+ console.log(b)*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /* Strings */
+ // Question-26 : Create variables for first name and last name and combine them using concatenation.
+ /*let name= "Janhavi"
+ let last= "Dubey"
+ console.log(name + last)*/
+
+ // Question-27(this is done) :  Display your introduction using template literals. 
+
+
+ // Question-28 : Find and print the length of the string "JavaScript"
+ /*let str= "JavaScript"
+ let a =(str.length)
+ console.log(a)*/
+
+
+
+
+ // Question-29 : Convert the string "hello world" into uppercase.
+ /*let str="hello world"
+ let a=(str.toUpperCase())
+ console.log(a)*/
+
+
+
+
+ // Question-30 :Convert the string "HELLO WORLD" into lowercase.
+/*let str= "HELLO WORLD"
+let a= (str.toLowerCase())
+console.log(a)*/
+
+
+
+
+// Question-31 :Extract the word "Java" from the string "JavaScript"
+/*let str= "JavaScript"
+let a= (str.slice(0,4))
+console.log(a)*/
+
+
+
+
+// Question-32 : Check whether the string "I love coding" contains the word "coding"
+/*let str= "I love coding"
+let a=(str.includes("coding"))
+console.log(a)*/
+
+
+
+
+// Question-33 : Replace the word "bad" with "good" in a sentence.
+/*let s= "bad"
+let b= (s.replace("good"))
+console.log(b)*/
+
+
+
+
+//  Question-34 :Remove unnecessary spaces from the beginning and end of a string.
+/*let str= "      HEYYYY    "
+let s=(str.trim())
+console.log(s)*/
+
+
+
+
+// Question-35 : Repeat the word "Hello" five times using a string method.
+let str= "Hello"
+let a= (str.repeat(5))
+console.log(a)
+
+
+
+
+
+
+
