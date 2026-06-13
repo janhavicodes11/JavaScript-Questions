8b9a4b48986cc06cfd5c3c1aa14d76f9cd989e68
Janhavi <janhavi.hariom2026@gmail.com>
2026-06-13 23:55:38 +0530

    Add files via upload

---
+++ Phase-1/conditionals/script.js
@@ -0,0 +1,27 @@
+// Question-36 : Write a program that checks whether a number entered by the user is positive, negative, or zero.
+/*let num= Number(prompt("Enter a num :"))
+if(num>0){
+    console.log("Its positive")
+}
+
+if(num<0){
+    console.log("its negetive")
+}else{
+    console.log("zero")
+}*/
+
+
+
+
+// Question-37 : Write a program that determines whether a number is even or odd.
+/*let  num= Number(prompt("Enter a number : "))
+if(num%2==0){
+    console.log("its even")
+}else{
+    console.log("its odd")
+}*/
+
+
+
+
+// Question-38 : Ask the user for their age and determine whether they are eligible to vote.
