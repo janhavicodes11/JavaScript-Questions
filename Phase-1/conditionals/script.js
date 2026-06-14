bc448a44dae1c96ba7fb368690117b12da404f27
Janhavi <janhavi.hariom2026@gmail.com>
2026-06-14 23:40:00 +0530

    Update script.js

--- Phase-1/conditionals/script.js
+++ Phase-1/conditionals/script.js
@@ -25,3 +25,66 @@ if(num%2==0){
 
 
 // Question-38 : Ask the user for their age and determine whether they are eligible to vote.
+/*let age=Number(prompt("Enter your age : "))
+if(age>=18){
+    console.log("You can vote")
+}else{
+    console.log("You can not vote")
+}*/
+
+
+
+
+// Question-39 : Write a program to determine whether a given year is a leap year.
+/*let year= Number(prompt("Enter year : "))
+if(year%400==0){
+    console.log("Its a leap year")
+}else if (year%4==0 && year%100 !== 0){
+    console.log("its a leap year")
+} else{
+    console.log("not a leap year")
+}*/
+
+
+
+
+// Question-40 : Compare two numbers and display the larger one.
+/*var num1= Number(prompt("Enter num 1 :"))
+var num2=Number(prompt("Enter num 2 : "))
+if(num1>num2){
+    console.log(num1, "Num 1 is greater")
+}else{
+    console.log(num2, "Number 2 is greater ")
+}*/
+
+
+
+
+// Question-41 :  Compare three numbers and display the largest among them. 
+/*let num1= Number(prompt("Enter num 1 : "))
+let num2= Number(prompt("Enter num 2 : "))
+let num3= Number(prompt("Enter num 3 : "))
+if(num1>num2){
+    console.log(num1, "Num 1 is greater")
+}else if(num2>num3) {
+    console.log(num2, "Num 2 is greater")
+}else{
+    console.log(num3, "Num 3 is greater")
+}*/
+
+
+
+
+// Question-42 : Ask the user for marks and assign grades according to predefined criteria.
+/*let marks=Number(prompt("Enter your marks"))
+if(marks>=90 && marks<=100){
+    console.log(marks, "A")
+}else if(marks>=80 && marks<=85){
+    console.log(marks, "B")
+}else if(marks>=70 && marks<=75){
+    console.log(marks, "C")
+}else if(marks>=60 && marks<=65){
+    console.log(marks, "D")
+}else{
+    console.log(marks, "E")
+}*/
