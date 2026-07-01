ba5b3aec74b9a19cefab36e00e8b39305383d9cb
Janhavi <janhavi.hariom2026@gmail.com>
2026-07-02 01:52:47 +0530

    Update script.js

--- Phase-1/conditionals/script.js
+++ Phase-1/conditionals/script.js
@@ -1,5 +1,5 @@
 // Question-36 : Write a program that checks whether a number entered by the user is positive, negative, or zero.
-/*let num= Number(prompt("Enter a num :"))
+let num= Number(prompt("Enter a num :"))
 if(num>0){
     console.log("Its positive")
 }
@@ -8,60 +8,60 @@ if(num<0){
     console.log("its negetive")
 }else{
     console.log("zero")
-}*/
+}
 
 
 
 
 // Question-37 : Write a program that determines whether a number is even or odd.
-/*let  num= Number(prompt("Enter a number : "))
+let  num= Number(prompt("Enter a number : "))
 if(num%2==0){
     console.log("its even")
 }else{
     console.log("its odd")
-}*/
+}
 
 
 
 
 // Question-38 : Ask the user for their age and determine whether they are eligible to vote.
-/*let age=Number(prompt("Enter your age : "))
+let age=Number(prompt("Enter your age : "))
 if(age>=18){
     console.log("You can vote")
 }else{
     console.log("You can not vote")
-}*/
+}
 
 
 
 
 // Question-39 : Write a program to determine whether a given year is a leap year.
-/*let year= Number(prompt("Enter year : "))
+let year= Number(prompt("Enter year : "))
 if(year%400==0){
     console.log("Its a leap year")
 }else if (year%4==0 && year%100 !== 0){
     console.log("its a leap year")
 } else{
     console.log("not a leap year")
-}*/
+}
 
 
 
 
 // Question-40 : Compare two numbers and display the larger one.
-/*var num1= Number(prompt("Enter num 1 :"))
+var num1= Number(prompt("Enter num 1 :"))
 var num2=Number(prompt("Enter num 2 : "))
 if(num1>num2){
     console.log(num1, "Num 1 is greater")
 }else{
     console.log(num2, "Number 2 is greater ")
-}*/
+}
 
 
 
 
 // Question-41 :  Compare three numbers and display the largest among them. 
-/*let num1= Number(prompt("Enter num 1 : "))
+let num1= Number(prompt("Enter num 1 : "))
 let num2= Number(prompt("Enter num 2 : "))
 let num3= Number(prompt("Enter num 3 : "))
 if(num1>num2){
@@ -70,7 +70,7 @@ if(num1>num2){
     console.log(num2, "Num 2 is greater")
 }else{
     console.log(num3, "Num 3 is greater")
-}*/
+}
 
 
 
@@ -134,6 +134,39 @@ if(num%5===0 && num%11===0){
 }else{
     console.log("its not divisble")
 }
+
+
+
+
+
+// SWITCH-STATEMENTS :
+ // Question-46 : Ask the user for a day number (1–7) and display the corresponding weekday.
+ let day=Number(prompt("Enter a day number"))
+ switch(day){
+    case 1:
+    console.log("its monday ");
+    break;
+    case 2:
+    console.log("its tuesday ");
+    break;
+    case 3:
+    console.log("its wed ");
+    break;
+    case 4:
+    console.log("its thu");
+    break;
+    case 5:
+    console.log("its friday ");
+    break;
+    case 6:
+    console.log("its sat ");
+    break;
+    case 7:
+    console.log("its sun");
+    break;
+     default:
+        console.log("wrong  day number");
+ }
 
 
 
