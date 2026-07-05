2e8ebfe533c0a445c46dd90886c7aa9d60f297c8
Janhavi <janhavi.hariom2026@gmail.com>
2026-07-05 23:57:22 +0530

    Add files via upload

---
+++ Phase-1/Loops/script.js
@@ -0,0 +1,40 @@
+// Question-51 : Print all numbers from 1 to 100 using a loop.
+/*for(i=1;i<=100;i++){
+    console.log(i)
+}*/
+
+
+
+
+// Question-52 : Print all even numbers between 1 and 100.
+/*for(i=1;i<=100;i++){
+    if(i%2==0){
+        console.log(i)
+    }
+    
+}*/
+
+
+
+
+//  Question-53 : Print all odd numbers between 1 and 100.
+/*for(i=1;i<=100;i++){
+    if(i%2!==0){
+        console.log(i)
+    }
+}*/
+
+
+
+
+// Question-54 : Generate the multiplication table of a number entered by the user.
+let num1=Number(prompt("Enter a num"))
+for(i=1;i<=10;i++){
+    console.log(num1*i)
+}
+
+
+
+
+
+// Question-55 : Find the factorial of a number entered by the user.
