5dc05c4d5780a57b7956a39e1b27e4169270cca4
Janhavi <janhavi.hariom2026@gmail.com>
2026-07-07 23:34:40 +0530

    Add files via upload

---
+++ Phase-1/Patterns/script.js
@@ -0,0 +1,13 @@
+// Question-61 : Print a square pattern of stars of size 5×5.
+for(let i=1;i<=5;i++){
+   let pattern= ""
+   for(let j=1;j<=5;j++){
+     pattern=pattern+"*"
+   }
+   console.log(pattern)
+}
+
+
+
+
+// Question-62 : Print a rectangular star pattern of 4 rows and 6 columns.
