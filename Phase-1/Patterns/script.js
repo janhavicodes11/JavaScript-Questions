06f1ab99109e4bbb3b37219882a7eae69476b430
Janhavi <janhavi.hariom2026@gmail.com>
2026-07-10 23:03:18 +0530

    Update script.js

--- Phase-1/Patterns/script.js
+++ Phase-1/Patterns/script.js
@@ -11,38 +11,38 @@ for(let i=1;i<=5;i++){
 
 
 // Question-62 : Print a rectangular star pattern of 4 rows and 6 columns.
-/*for(let i=0;i<4;i++){
+for(let i=0;i<4;i++){
      let pattern=""
     for(let j=1;j<6;j++){
         pattern=pattern+"*"
     }
     console.log(pattern)
-}*/
+}
 
 
 
 
 // Question-63 : Print a right-angled triangle star pattern.
-/*for(let i=1;i<=5;i++){
+for(let i=1;i<=5;i++){
     let pattern=""
     for(j=1;j<=i;j++){
         pattern=pattern+"*"
     }
     console.log(pattern)
-}*/
+}
 
 
 
 
 
 // Question-64 : Print an inverted right-angled triangle pattern.
-/*for(i=5;i>=1;i--){
+for(i=5;i>=1;i--){
     let pattern= ""
     for(j=1;j<=i;j++){
         pattern=pattern+"*"
     }
     console.log(pattern)
-}*/
+}
 
 
 
