42b96c9d95b7e0cf43503c334a88ee883bfa017e
Janhavi <janhavi.hariom2026@gmail.com>
2026-09-01 23:54:39 +0530

    Update script.js

--- Phase-2/Functions/Q14- Counts vowels in a string/script.js
+++ Phase-2/Functions/Q14- Counts vowels in a string/script.js
@@ -1,4 +1,19 @@
-function vowel(a){
-let count=0
-for(let i=0; i)
+function vowel(str) {
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (
+            str[i] === 'a' ||
+            str[i] === 'e' ||
+            str[i] === 'i' ||
+            str[i] === 'o' ||
+            str[i] === 'u'
+        ) {
+            count += 1;
+        }
+    }
+
+    return count;
 }
+
+console.log(vowel("Janhavi")); // 3
